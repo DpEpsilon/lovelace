693a317b00efed1efbc503a2e03256f986f36350
Christopher Chen <chris@openendings.net>
2015-12-31 13:58:29 +1100

    Javadocs

--- src/main/scala/train/db/DatabaseInterface.scala
+++ src/main/scala/train/db/DatabaseInterface.scala
@@ -6,7 +6,9 @@ import scalaz.concurrent.Task
 import train.data._
 
 trait DatabaseInterface {
+  // verifyStudent(id, password) returns true iff the username/password pair is valid
   def verifyStudent : (StudentID, String) => Task[Boolean]
+
   def getProblemsForStudent : StudentID => Task[Seq[Problem]]
   def getProblemSetsForStudent : StudentID => Task[Seq[ProblemSet]]
   def getSubmissionCode : (StudentID, ProblemID, Int) => Task[Option[Code]]
