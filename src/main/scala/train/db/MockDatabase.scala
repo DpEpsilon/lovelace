d99b0127f211712498a6262adcb45b752f9ae89b
Christopher Chen <chris@openendings.net>
2015-12-31 13:31:37 +1100

    MockDatabase class

---
+++ src/main/scala/train/db/MockDatabase.scala
@@ -0,0 +1,24 @@
+package train.db
+
+import scala.language.higherKinds
+import scalaz.concurrent.Task
+
+import train.data._
+
+/**
+ * A trivial read-only database containing no data and failing all operations.
+ * Intended for smoke tests on the server.
+ */
+class MockDatabase extends DatabaseInterface[Task] {
+  type DBQuery[A] = Task[A]
+  type DBWrite[A] = Task[A]
+
+  implicit def asTask[A] = identity
+
+  def verifyStudent = (id: StudentID, password: String) => Task.delay(None)
+  def getProblemsForStudent = (id: StudentID) => Task.delay(Seq.empty)
+  def getProblemSetsForStudent = (id: StudentID) => Task.delay(Seq.empty)
+  def getSubmissionCode = (id: StudentID, problemId: ProblemID, n: Int) => Task.delay(None)
+  def getSubmissionResult = (id: StudentID, problemId: ProblemID, n: Int) => Task.delay(None)
+  def addSubmission = (id: StudentID, problemId: ProblemID, code: Code) => Task.fail(new RuntimeException("Could not access database"))
+}
