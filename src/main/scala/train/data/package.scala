5d2833f0ad2598588e56e7565623a410e331c2b8
Christopher Chen <chris@openendings.net>
2016-01-01 15:16:40 +1100

    Add comments for train.data

--- src/main/scala/train/data/package.scala
+++ src/main/scala/train/data/package.scala
@@ -1,12 +1,18 @@
 package train
 
-
+/**
+ * Data types for training site "business logic".
+ *
+ * Does *not* include: data types specific to a web frontend / JSON API / etc.
+ */
 object data {
+  // IDs and unique identifiers
   case class StudentID(val id: Int)
   case class ProblemID(val id: Int)
   case class Problem(val id: ProblemID)
-  case class Submission(val code: Code, val result: SubmissionResult)
   case class ProblemSet()
+
+  case class Submission(val code: Code, val result: SubmissionResult)
   case class Code()
   case class SubmissionResult()
 }
