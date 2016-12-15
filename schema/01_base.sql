f346c094a87674ec9d1ba00e64c95cb13960289e
Daniel Phillips <dp@premonition.io>
2016-12-15 19:01:53 +1100

    Make keywords all caps

--- schema/01_base.sql
+++ schema/01_base.sql
@@ -10,135 +10,135 @@ INSERT INTO _meta (version) VALUES (1);
 
 
 CREATE TABLE access_sets (
-    competitorid integer NOT NULL REFERENCES competitors(id),
-    set character varying(40) NOT NULL REFERENCES sets(name),
-    granted timestamp with time zone,
+    competitorid INTEGER NOT NULL REFERENCES competitors(id),
+    set VARCHAR(40) NOT NULL REFERENCES sets(name),
+    granted TIMESTAMP WITH TIME ZONE,
     PRIMARY KEY (competitorid, set)
 );
 
 CREATE TABLE competitors (
     id SERIAL PRIMARY KEY,
-    username character varying(15) NOT NULL,
-    password character varying(15) NOT NULL,
-    firstname character varying(40) NOT NULL,
-    lastname character varying(40) NOT NULL,
-    defaultlang character varying(10) REFERENCES languages(id),
-    email character varying(255) NOT NULL,
-    school character varying(255) NOT NULL,
-    year character varying(10) NOT NULL,
-    phone character varying(40),
-    address character varying(512),
-    state character varying(15) NOT NULL,
-    country character varying(255) NOT NULL,
-    registered timestamp with time zone NOT NULL,
-    registeredip character varying(50) NOT NULL,
-    approved timestamp with time zone,
-    approvedby character varying(15),
-    lastseen timestamp with time zone,
-    lastsubmitted timestamp with time zone,
-    fullaccess boolean DEFAULT false,
-    suspended boolean DEFAULT false,
-    optshowdone boolean DEFAULT false,
-    opensource boolean DEFAULT true
+    username VARCHAR(15) NOT NULL,
+    password VARCHAR(15) NOT NULL,
+    firstname VARCHAR(40) NOT NULL,
+    lastname VARCHAR(40) NOT NULL,
+    defaultlang VARCHAR(10) REFERENCES languages(id),
+    email VARCHAR(255) NOT NULL,
+    school VARCHAR(255) NOT NULL,
+    year VARCHAR(10) NOT NULL,
+    phone VARCHAR(40),
+    address VARCHAR(512),
+    state VARCHAR(15) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    registered TIMESTAMP WITH TIME ZONE NOT NULL,
+    registeredip VARCHAR(50) NOT NULL,
+    approved TIMESTAMP WITH TIME ZONE,
+    approvedby VARCHAR(15),
+    lastseen TIMESTAMP WITH TIME ZONE,
+    lastsubmitted TIMESTAMP WITH TIME ZONE,
+    fullaccess BOOLEAN DEFAULT false,
+    suspended BOOLEAN DEFAULT false,
+    optshowdone BOOLEAN DEFAULT false,
+    opensource BOOLEAN DEFAULT true
 );
 
 CREATE TABLE problems (
     id SERIAL PRIMARY KEY,
-    name character varying(40) NOT NULL,
-    title character varying(255) NOT NULL,
-    files character varying(255)
+    name VARCHAR(40) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    files VARCHAR(255)
 );
 
 CREATE TABLE set_contents (
-    set character varying(40) NOT NULL REFERENCES sets(name),
-    problemid integer NOT NULL REFERENCES problems(id),
-    "order" integer,
+    set VARCHAR(40) NOT NULL REFERENCES sets(name),
+    problemid INTEGER NOT NULL REFERENCES problems(id),
+    "order" INTEGER,
     PRIMARY KEY (set, problemid)
 );
 
 CREATE TABLE sets (
-    name character varying(40) PRIMARY KEY,
-    title character varying(255) NOT NULL,
-    public boolean DEFAULT false,
-    priority integer
+    name VARCHAR(40) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    public BOOLEAN DEFAULT false,
+    priority INTEGER
 );
 
 CREATE TABLE access_prereqs (
-    set character varying(40) NOT NULL REFERENCES sets(name),
-    requires character varying(40) NOT NULL REFERENCES sets(name),
-    percentage integer NOT NULL,
+    set VARCHAR(40) NOT NULL REFERENCES sets(name),
+    requires VARCHAR(40) NOT NULL REFERENCES sets(name),
+    percentage INTEGER NOT NULL,
     PRIMARY KEY (set, requires)
 );
 
 CREATE TABLE fame_problems (
-    problemid integer PRIMARY KEY REFERENCES problems(id),
-    set character varying(40) NOT NULL REFERENCES sets(name),
-    type character(1) DEFAULT 't'::bpchar NOT NULL
+    problemid INTEGER PRIMARY KEY REFERENCES problems(id),
+    set VARCHAR(40) NOT NULL REFERENCES sets(name),
+    type CHAR(1) DEFAULT 't'::bpchar NOT NULL
 );
 
 CREATE TABLE languages (
-    id character varying(10) PRIMARY KEY,
-    name character varying(40) NOT NULL
+    id VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE notify_prereqs (
-    set character varying(40) NOT NULL REFERENCES sets(name),
-    requires character varying(40) NOT NULL REFERENCES sets(name),
+    set VARCHAR(40) NOT NULL REFERENCES sets(name),
+    requires VARCHAR(40) NOT NULL REFERENCES sets(name),
     PRIMARY KEY (set, requires)
 );
 
 CREATE TABLE progress (
-    competitorid integer NOT NULL REFERENCES competitors(id),
-    problemid integer NOT NULL REFERENCES problems(id),
-    viewed timestamp with time zone NOT NULL,
-    bestscore integer,
-    bestscoreon timestamp with time zone,
-    "time" real,
+    competitorid INTEGER NOT NULL REFERENCES competitors(id),
+    problemid INTEGER NOT NULL REFERENCES problems(id),
+    viewed TIMESTAMP WITH TIME ZONE NOT NULL,
+    bestscore INTEGER,
+    bestscoreon TIMESTAMP WITH TIME ZONE,
+    "time" REAL,
     PRIMARY KEY (competitorid, problemid)
 );
 
 CREATE TABLE result_types (
-    id character varying(10) PRIMARY KEY,
-    name character varying(255) NOT NULL,
-    explanation text
+    id VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    explanation TEXT
 );
 
 CREATE TABLE results (
-    competitorid integer NOT NULL,
-    problemid integer NOT NULL,
-    attempt integer NOT NULL,
-    testcaseid character varying(40) NOT NULL,
-    resultid character varying(10) NOT NULL REFERENCES result_types(id),
-    mark integer NOT NULL,
+    competitorid INTEGER NOT NULL,
+    problemid INTEGER NOT NULL,
+    attempt INTEGER NOT NULL,
+    testcaseid VARCHAR(40) NOT NULL,
+    resultid VARCHAR(10) NOT NULL REFERENCES result_types(id),
+    mark INTEGER NOT NULL,
     PRIMARY KEY (competitorid, problemid, attempt, testcaseid),
     CONSTRAINT results_attempt FOREIGN KEY (competitorid, problemid, attempt)
         REFERENCES submissions(competitorid, problemid, attempt),
 );
 
 CREATE TABLE status_types (
-    id character varying(15) PRIMARY KEY,
-    name character varying(255) NOT NULL,
-    explanation text
+    id VARCHAR(15) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    explanation TEXT
 );
 
 CREATE TABLE submissions (
-    competitorid integer NOT NULL REFERENCES competitors(id),
-    problemid integer NOT NULL REFERENCES problems(id),
-    attempt integer NOT NULL,
-    languageid character varying(10) NOT NULL REFERENCES languages(id),
-    statusid character varying(15) REFERENCES status_types(id),
-    submitted_file text NOT NULL,
-    "timestamp" timestamp with time zone NOT NULL,
-    ipaddress character varying(50),
-    mark integer,
-    comment text,
-    judge text,
+    competitorid INTEGER NOT NULL REFERENCES competitors(id),
+    problemid INTEGER NOT NULL REFERENCES problems(id),
+    attempt INTEGER NOT NULL,
+    languageid VARCHAR(10) NOT NULL REFERENCES languages(id),
+    statusid VARCHAR(15) REFERENCES status_types(id),
+    submitted_file TEXT NOT NULL,
+    "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL,
+    ipaddress VARCHAR(50),
+    mark INTEGER,
+    comment TEXT,
+    judge TEXT,
     PRIMARY KEY (competitorid, problemid, attempt)
 );
 
 CREATE TABLE watchers (
-    competitorid integer NOT NULL REFERENCES competitors(id),
-    email character varying(255) NOT NULL,
+    competitorid INTEGER NOT NULL REFERENCES competitors(id),
+    email VARCHAR(255) NOT NULL,
     PRIMARY KEY (competitorid, email)
 );
 
