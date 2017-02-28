3246d6a42a374cc165588a58eaf517b96141e65c
Daniel Phillips <dyproepsilon@gmail.com>
2017-02-28 14:19:31 +1100

    Rename more columns

--- schema/01_base.sql
+++ schema/01_base.sql
@@ -10,36 +10,36 @@ INSERT INTO _meta (version) VALUES (1);
 
 
 CREATE TABLE access_sets (
-    competitorid INTEGER NOT NULL REFERENCES competitors(id),
-    set          TEXT NOT NULL REFERENCES sets(name),
-    granted      TIMESTAMP WITH TIME ZONE,
-    PRIMARY KEY (competitorid, set)
+    competitor INTEGER NOT NULL REFERENCES competitors(id),
+    set        TEXT NOT NULL REFERENCES sets(name),
+    granted    TIMESTAMP WITH TIME ZONE,
+    PRIMARY KEY (competitor, set)
 );
 
 CREATE TABLE competitors (
-    id            BIGSERIAL PRIMARY KEY,
-    username      TEXT NOT NULL,
-    password      TEXT NOT NULL,
-    firstname     TEXT NOT NULL,
-    lastname      TEXT NOT NULL,
-    defaultlang   TEXT REFERENCES languages(id),
-    email         TEXT NOT NULL,
-    school        TEXT NOT NULL,
-    year          TEXT NOT NULL,
-    phone         TEXT,
-    address       TEXT,
-    state         TEXT NOT NULL,
-    country       TEXT NOT NULL,
-    registered    TIMESTAMP WITH TIME ZONE NOT NULL,
-    registeredip  TEXT NOT NULL,
-    approved      TIMESTAMP WITH TIME ZONE,
-    approvedby    TEXT,
-    lastseen      TIMESTAMP WITH TIME ZONE,
-    lastsubmitted TIMESTAMP WITH TIME ZONE,
-    fullaccess    BOOLEAN DEFAULT false,
-    suspended     BOOLEAN DEFAULT false,
-    optshowdone   BOOLEAN DEFAULT false,
-    opensource    BOOLEAN DEFAULT true
+    id             BIGSERIAL PRIMARY KEY,
+    username       TEXT NOT NULL,
+    password       TEXT NOT NULL,
+    firstname      TEXT NOT NULL,
+    lastname       TEXT NOT NULL,
+    default_lang   TEXT REFERENCES languages(id),
+    email          TEXT NOT NULL,
+    school         TEXT NOT NULL,
+    year           TEXT NOT NULL,
+    phone          TEXT,
+    address        TEXT,
+    state          TEXT NOT NULL,
+    country        TEXT NOT NULL,
+    registered     TIMESTAMP WITH TIME ZONE NOT NULL,
+    registered_ip  TEXT NOT NULL,
+    approved       TIMESTAMP WITH TIME ZONE,
+    approved_by    TEXT,
+    last_seen      TIMESTAMP WITH TIME ZONE,
+    last_submitted TIMESTAMP WITH TIME ZONE,
+    full_access    BOOLEAN DEFAULT false,
+    suspended      BOOLEAN DEFAULT false,
+    show_done      BOOLEAN DEFAULT false,
+    open_source    BOOLEAN DEFAULT true
 );
 
 CREATE TABLE problems (
@@ -50,10 +50,10 @@ CREATE TABLE problems (
 );
 
 CREATE TABLE set_contents (
-    set       TEXT NOT NULL REFERENCES sets(name),
-    problemid INTEGER NOT NULL REFERENCES problems(id),
-    priority  INTEGER,
-    PRIMARY KEY (set, problemid)
+    set      TEXT NOT NULL REFERENCES sets(name),
+    problem  INTEGER NOT NULL REFERENCES problems(id),
+    priority INTEGER,
+    PRIMARY KEY (set, problem)
 );
 
 CREATE TABLE sets (
@@ -64,16 +64,16 @@ CREATE TABLE sets (
 );
 
 CREATE TABLE access_prereqs (
-    set        TEXT NOT NULL REFERENCES sets(name),
-    requires   TEXT NOT NULL REFERENCES sets(name),
-    percentage INTEGER NOT NULL,
+    set         TEXT NOT NULL REFERENCES sets(name),
+    requirement TEXT NOT NULL REFERENCES sets(name),
+    percentage  INTEGER NOT NULL,
     PRIMARY KEY (set, requires)
 );
 
 CREATE TABLE fame_problems (
-    problemid INTEGER PRIMARY KEY REFERENCES problems(id),
-    set       TEXT NOT NULL REFERENCES sets(name),
-    type      TEXT DEFAULT 't' NOT NULL
+    problem INTEGER PRIMARY KEY REFERENCES problems(id),
+    set     TEXT NOT NULL REFERENCES sets(name),
+    type    TEXT DEFAULT 't' NOT NULL
 );
 
 CREATE TABLE languages (
@@ -82,19 +82,19 @@ CREATE TABLE languages (
 );
 
 CREATE TABLE notify_prereqs (
-    set      TEXT NOT NULL REFERENCES sets(name),
-    requires TEXT NOT NULL REFERENCES sets(name),
+    set         TEXT NOT NULL REFERENCES sets(name),
+    requirement TEXT NOT NULL REFERENCES sets(name),
     PRIMARY KEY (set, requires)
 );
 
 CREATE TABLE progress (
-    competitorid INTEGER NOT NULL REFERENCES competitors(id),
-    problemid    INTEGER NOT NULL REFERENCES problems(id),
-    viewed       TIMESTAMP WITH TIME ZONE NOT NULL,
-    bestscore    INTEGER,
-    bestscoreon  TIMESTAMP WITH TIME ZONE,
+    competitor    INTEGER NOT NULL REFERENCES competitors(id),
+    problem       INTEGER NOT NULL REFERENCES problems(id),
+    viewed        TIMESTAMP WITH TIME ZONE NOT NULL,
+    best_score    INTEGER,
+    best_score_at TIMESTAMP WITH TIME ZONE,
     time_taken   INTEGER, -- milliseconds
-    PRIMARY KEY (competitorid, problemid)
+    PRIMARY KEY (competitor, problem)
 );
 
 CREATE TABLE result_types (
@@ -104,15 +104,15 @@ CREATE TABLE result_types (
 );
 
 CREATE TABLE results (
-    competitorid INTEGER NOT NULL,
-    problemid    INTEGER NOT NULL,
-    attempt      INTEGER NOT NULL,
-    testcaseid   TEXT NOT NULL,
-    resultid     TEXT NOT NULL REFERENCES result_types(id),
-    mark         INTEGER NOT NULL,
-    PRIMARY KEY (competitorid, problemid, attempt, testcaseid),
-    FOREIGN KEY (competitorid, problemid, attempt)
-        REFERENCES submissions(competitorid, problemid, attempt),
+    competitor INTEGER NOT NULL,
+    problem    INTEGER NOT NULL,
+    attempt    INTEGER NOT NULL,
+    test_case  TEXT NOT NULL,
+    result     TEXT NOT NULL REFERENCES result_types(id),
+    mark       INTEGER NOT NULL,
+    PRIMARY KEY (competitor, problem, attempt, testcase),
+    FOREIGN KEY (competitor, problem, attempt)
+        REFERENCES submissions(competitor, problem, attempt),
 );
 
 CREATE TABLE status_types (
@@ -122,24 +122,24 @@ CREATE TABLE status_types (
 );
 
 CREATE TABLE submissions (
-    competitorid   INTEGER NOT NULL REFERENCES competitors(id),
-    problemid      INTEGER NOT NULL REFERENCES problems(id),
+    competitor     INTEGER NOT NULL REFERENCES competitors(id),
+    problem        INTEGER NOT NULL REFERENCES problems(id),
     attempt        INTEGER NOT NULL,
-    languageid     TEXT NOT NULL REFERENCES languages(id),
-    statusid       TEXT REFERENCES status_types(id),
+    language       TEXT NOT NULL REFERENCES languages(id),
+    status         TEXT REFERENCES status_types(id),
     submitted_file TEXT NOT NULL,
     created_at     TIMESTAMP WITH TIME ZONE NOT NULL,
-    ipaddress      TEXT,
+    ip_address     TEXT,
     mark           INTEGER,
     comment        TEXT,
-    judge          TEXT,
-    PRIMARY KEY (competitorid, problemid, attempt)
+    judge_output   TEXT,
+    PRIMARY KEY (competitor, problem, attempt)
 );
 
 CREATE TABLE watchers (
-    competitorid INTEGER NOT NULL REFERENCES competitors(id),
-    email        TEXT NOT NULL,
-    PRIMARY KEY (competitorid, email)
+    competitor INTEGER NOT NULL REFERENCES competitors(id),
+    email      TEXT NOT NULL,
+    PRIMARY KEY (competitor, email)
 );
 
 COMMIT;
